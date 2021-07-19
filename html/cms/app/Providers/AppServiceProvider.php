6e40a17afed2e52a249fad04ff54cf68c1262f94
fjsho <gorimura7@gmail.com>
2021-07-19 20:21:13 +0900

    [fix]設定ファイルの変更

--- html/cms/app/Providers/AppServiceProvider.php
+++ html/cms/app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        schema::defaultStringLength(191);
+        url::forceScheme('https');
     }
 }
