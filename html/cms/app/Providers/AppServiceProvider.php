98ac31b25c1c415b800f919ec458f4d34a543357
fjsho <gorimura7@gmail.com>
2021-07-21 21:48:18 +0900

    [fix]5章の不要な記述を削除

--- html/cms/app/Providers/AppServiceProvider.php
+++ html/cms/app/Providers/AppServiceProvider.php
@@ -26,7 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        schema::defaultStringLength(191);
-        url::forceScheme('https');
+        //
     }
 }
