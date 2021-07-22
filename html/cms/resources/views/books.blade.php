e9114c2158c0593aee2f6ef37715ea5056b99f13
fjsho <gorimura7@gmail.com>
2021-07-22 21:22:32 +0900

    [add]削除・更新機能の実装

--- html/cms/resources/views/books.blade.php
+++ html/cms/resources/views/books.blade.php
@@ -17,8 +17,25 @@
     
       <!-- 本のタイトル -->
       <div class="form-group">
-        <div class="col-sm-6">
-          <input type="text" name="item_name" class="form-control">
+        <div class="row">
+          <div class="col-sm-6">
+            Book
+            <input type="text" name="item_name" class="form-control">
+          </div>          
+          <div class="col-sm-6">
+            冊数
+            <input type="text" name="item_number" class="form-control">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-6">
+            金額
+            <input type="text" name="item_amount" class="form-control">
+          </div>          
+          <div class="col-sm-6">
+            公開日
+            <input type="text" name="published" class="form-control" placeholder="年/月/日">
+          </div>
         </div>
       </div>
 
@@ -32,6 +49,58 @@
       </div>
     </form>
   </div>
+
   <!-- Book：すでに登録されている本のリスト -->
+  <!-- 現在の本 -->
+  @if (count($books) > 0)
+    <div class="card-body">
+      <div class="card-body">
+        <table class="table table-striped task-table">
+          <!-- テーブルヘッダ -->
+          <thead>
+            <th>本一覧</th>
+            <th>&nbsp;</th>
+            <th>&nbsp;</th>
+          </thead>
+          <!-- テーブル本体 -->
+          <tbody>
+            @foreach ($books as $book)
+              <tr>
+                <!-- 本タイトル -->
+                <td class="table-text">
+                  <div>{{ $book->item_name }}</div>
+                </td>
+
+                <!-- 本: 更新ボタン -->
+                <td>
+                  <form action="{{ url('booksedit/'.$book->id) }}" method="POST">
+                    @csrf               <!-- CSRFからの保護 -->
+                    
+                    <button type="submit" class="btn btn-primary">
+                        更新
+                    </button>
+                  </form>
+                </td>
+
+                <!-- 本: 削除ボタン -->
+                <td>
+                  <form action="{{ url('book/'.$book->id) }}" method="POST">
+                    @csrf               <!-- CSRFからの保護 -->
+                    @method('DELETE')   <!-- 擬似フォームメソッド -->
+                    
+                    <button type="submit" class="btn btn-danger">
+                        削除
+                    </button>
+                  </form>
+                </td>
+              </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+    </div>
+  @endif
+
+
 
 @endsection
--- html/cms/routes/web.php
+++ html/cms/routes/web.php
@@ -18,16 +18,25 @@ use Illuminate\Http\Request;
 * 本の一覧表示(books.blade.php)
 */
 Route::get('/', function () {
-    return view('books');
+    $books = Book::orderBy('created_at', 'asc')->get();
+    return view('books', [
+        'books' => $books
+    ]);
+    //return view('books',compact('books')); //も同じ意味
 });
 
+
 /**
-* 本を追加 
+* 本を登録
 */
 Route::post('/books', function (Request $request) {
 
     //バリデーション
-    $validator = Validator::make($request->all(),['item_name' => 'required|max:255',
+    $validator = Validator::make($request->all(),[
+        'item_name' => 'required|min:3|max:255',
+        'item_number' => 'required|min:1|max:255',
+        'item_amount' => 'required|max:6',
+        'published' => 'required|date',
     ]);
 
     //バリデーション：エラー
@@ -40,17 +49,53 @@ Route::post('/books', function (Request $request) {
     //Eloquintモデル（登録処理）
     $books = new Book;
     $books->item_name = $request->item_name;
-    $books->item_number = "1";
-    $books->item_amount = "1000";
-    $books->published = '2017-03-07 00:00:00';
+    $books->item_number = $request->item_number;
+    $books->item_amount = $request->item_amount;
+    $books->published = $request->published;
     $books->save();
+    return redirect('/');
 });
 
 /**
 * 本を削除 
 */
 Route::delete('/book/{book}', function (Book $book) {
-    //
+    $book->delete();
+    return redirect('/');
+});
+
+/**
+* 本を更新
+*/
+//更新画面
+Route::post('/booksedit/{books}', function (Book $books) {
+    return view('booksedit',['book'=>$books]);
+});
+//更新処理
+Route::post('/books/update', function (Request $request) {
+    //バリデーション
+    $validator = Validator::make($request->all(), [
+        'id' => 'required',
+        'item_name' => 'required|min:3|max:255',
+        'item_number' => 'required|min:1|max:3',
+        'item_amount' => 'required|max:6',
+        'published' => 'required',
+    ]);
+    //バリデーション:エラー
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    //データ更新
+    $books = Book::find($request->id);
+    $books->item_name   = $request->item_name;
+    $books->item_number = $request->item_number;
+    $books->item_amount = $request->item_amount;
+    $books->published   = $request->published;
+    $books->save();
+    return redirect('/');
 });
 
 
