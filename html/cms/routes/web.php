f87eb5a56ea6f95fb02e0f339dff2d6286b58c1d
fjsho <gorimura7@gmail.com>
2021-07-24 10:04:31 +0900

    [add]Booksコントローラーの実装（登録・更新処理をルート定義から移管）

--- html/cms/routes/web.php
+++ html/cms/routes/web.php
@@ -29,32 +29,7 @@ Route::get('/', function () {
 /**
 * 本を登録
 */
-Route::post('/books', function (Request $request) {
-
-    //バリデーション
-    $validator = Validator::make($request->all(),[
-        'item_name' => 'required|min:3|max:255',
-        'item_number' => 'required|min:1|max:255',
-        'item_amount' => 'required|max:6',
-        'published' => 'required|date',
-    ]);
-
-    //バリデーション：エラー
-    if($validator->fails()){
-        return redirect('/')
-            ->withInput()
-            ->withErrors($validator);
-    }
-
-    //Eloquintモデル（登録処理）
-    $books = new Book;
-    $books->item_name = $request->item_name;
-    $books->item_number = $request->item_number;
-    $books->item_amount = $request->item_amount;
-    $books->published = $request->published;
-    $books->save();
-    return redirect('/');
-});
+Route::post('/books', 'BooksController@store');
 
 /**
 * 本を削除 
@@ -72,31 +47,7 @@ Route::post('/booksedit/{books}', function (Book $books) {
     return view('booksedit',['book'=>$books]);
 });
 //更新処理
-Route::post('/books/update', function (Request $request) {
-    //バリデーション
-    $validator = Validator::make($request->all(), [
-        'id' => 'required',
-        'item_name' => 'required|min:3|max:255',
-        'item_number' => 'required|min:1|max:3',
-        'item_amount' => 'required|max:6',
-        'published' => 'required',
-    ]);
-    //バリデーション:エラー
-    if ($validator->fails()) {
-        return redirect('/')
-            ->withInput()
-            ->withErrors($validator);
-    }
-
-    //データ更新
-    $books = Book::find($request->id);
-    $books->item_name   = $request->item_name;
-    $books->item_number = $request->item_number;
-    $books->item_amount = $request->item_amount;
-    $books->published   = $request->published;
-    $books->save();
-    return redirect('/');
-});
+Route::post('/books/update', 'BooksController@update');
 
 
 Auth::routes();
